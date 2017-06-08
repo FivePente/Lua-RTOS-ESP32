71c1a5953a4772b3e9ea29b4e2eab4fb2067f0e4
wangjhm <wangjhm@163.com>
2017-06-08 21:21:03 +0800

    ppp

--- components/lua_rtos/Lua/modules/lppp.c
+++ components/lua_rtos/Lua/modules/lppp.c
@@ -1,36 +1,33 @@
-/*
- * pppos
- */
 #include "luartos.h"
 
-//#if CONFIG_LUA_RTOS_LUA_USE_PPP
+#if CONFIG_LUA_RTOS_LUA_USE_TMR
 
-#include "modules.h"
 #include "lua.h"
-#include "error.h"
 #include "lauxlib.h"
-#include "platform.h"
-#include <stdlib.h>
-#include <string.h>
-#include "uart.h"
+#include "modules.h"
 
+#include <unistd.h>
+#include <sys/delay.h>
 
-static int pppos_task_step(lua_State* L){
-    //tcpip_adapter_init();
-    //xTaskCreate(&pppos_client_task, "pppos_client_task", 2048, NULL, 5, NULL); 
+static int pppos_step( lua_State* L ) {
     return 0;
 }
 
-//class map
-static const LUA_REG_TYPE pppos_map[] = {
-    { LSTRKEY( "step" ),  LFUNCVAL( pppos_task_step )},
+static const LUA_REG_TYPE tmr_map[] = {
+    { LSTRKEY( "step" ),			LFUNCVAL( pppos_step ) },
     { LNILKEY, LNILVAL }
 };
 
+LUALIB_API int luaopen_ppp( lua_State *L ) {
+#if !LUA_USE_ROTABLE
+    luaL_newlib(L, ppp_map);
 
-int luaopen_pppos(lua_State* L) {
-    return 0;
+    return 1;
+#else
+	return 0;
+#endif
 }
 
-MODULE_REGISTER_MAPPED(PPPOS, pppos, pppos_map, luaopen_pppos);
-//#endif
+MODULE_REGISTER_MAPPED(PPP, ppp, ppp_map, luaopen_ppp);
+
+#endif
