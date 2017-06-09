183cd4d9c53816936096a46ee0d685978bce7fe1
wangjhm <wangjhm@163.com>
2017-06-09 20:46:56 +0800

    ppp

--- components/lua_rtos/Lua/modules/ppp.c
+++ components/lua_rtos/Lua/modules/ppp.c
@@ -305,7 +305,6 @@ static void pppos_client_task()
                 pppos_input_tcpip(ppp, (u8_t *)data, len);
             }
         }
-
     }
 }
 
@@ -392,8 +391,7 @@ static void pppos_client_task()
 
 static int ppp_task_step(lua_State* L){
     tcpip_adapter_init();
-    //xTaskCreate(&pppos_client_task, "pppos_client_task", 2048, NULL, 5, NULL); 
-    pppos_client_task();
+    xTaskCreate(&pppos_client_task, "pppos_client_task", 2048, NULL, 5, NULL); 
     return 0;
 }
 
