58fa93c3be50685b9e64ea0783a2cc0dd529a009
wangjhm <wangjhm@163.com>
2017-06-09 09:15:29 +0800

    pppp

--- components/lua_rtos/Lua/modules/ppp.c
+++ components/lua_rtos/Lua/modules/ppp.c
@@ -1,60 +1,28 @@
 #include "luartos.h"
 
-#include "modules.h"
 #include "lua.h"
-#include "error.h"
 #include "lauxlib.h"
-#include "platform.h"
-#include <stdlib.h>
-#include <string.h>
+#include "modules.h"
 
-typedef struct {
-	int uart_num;
-} ppp_user_data_t;
+#include <unistd.h>
 
 static int pppos_step( lua_State* L ) {
-    // Allocate userdata
-    ppp_user_data_t *user_data = (ppp_user_data_t *)lua_newuserdata(L, sizeof(ppp_user_data_t));
-    if (!user_data) {
-       	return luaL_exception(L, 1);
-    }
-
-    user_data->uart_num = 2;
-
-    luaL_getmetatable(L, "ppp.trans");
-    lua_setmetatable(L, -2);
-
-    return 1;
-}
-
-// Destructor
-static int pppos_trans_gc (lua_State *L) {
-	ppp_user_data_t *user_data = NULL;
-
-    user_data = (ppp_user_data_t *)luaL_testudata(L, 1, "ppp.trans");
-    if (user_data) {
-    }
-
     return 0;
 }
 
-static const LUA_REG_TYPE pppos_map[] = {
+static const LUA_REG_TYPE pppss_map[] = {
     { LSTRKEY( "step" ), LFUNCVAL( pppos_step ) },
     { LNILKEY, LNILVAL }
 };
 
+LUALIB_API int luaopen_pppss( lua_State *L ) {
+#if !LUA_USE_ROTABLE
+    luaL_newlib(L, pppss_map);
 
-//inst map
-static const LUA_REG_TYPE pppos_trans_map[] = {
-    { LSTRKEY( "__metatable" ),  	LROVAL  ( pppos_trans_map ) },
-	{ LSTRKEY( "__index"     ),   	LROVAL  ( pppos_trans_map ) },
-	{ LSTRKEY( "__gc"        ),   	LFUNCVAL  ( pppos_trans_gc ) },
-    { LNILKEY, LNILVAL}
-};
-
-LUALIB_API int luaopen_ppp( lua_State *L ) {
-    luaL_newmetarotable(L,"ppp.trans", (void *)pppos_trans_map);
-    return 0;
+    return 1;
+#else
+	return 0;
+#endif
 }
 
-MODULE_REGISTER_MAPPED(PPP, ppp, pppos_map, luaopen_ppp);
+MODULE_REGISTER_MAPPED(PPP, pppss, pppss_map, luaopen_pppss);
