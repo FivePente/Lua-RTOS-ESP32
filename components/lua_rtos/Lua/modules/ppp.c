e4b9b790efea4b63b6c949b5a92edca4d05e2eed
wangjhm <wangjhm@163.com>
2017-06-09 08:45:20 +0800

    pppp

--- components/lua_rtos/Lua/modules/ppp.c
+++ components/lua_rtos/Lua/modules/ppp.c
@@ -4,7 +4,33 @@
 #include "lauxlib.h"
 #include "modules.h"
 
+typedef struct {
+	int uart_num;
+} ppp_user_data_t;
+
 static int pppos_step( lua_State* L ) {
+    // Allocate userdata
+    vl53l0x_user_data_t *user_data = (vl53l0x_user_data_t *)lua_newuserdata(L, sizeof(vl53l0x_user_data_t));
+    if (!user_data) {
+       	return luaL_exception(L, I2C_ERR_NOT_ENOUGH_MEMORY);
+    }
+
+    user_data->uart_num = 2;
+
+    luaL_getmetatable(L, "ppp.trans");
+    lua_setmetatable(L, -2);
+
+    return 1;
+}
+
+// Destructor
+static int pppos_trans_gc (lua_State *L) {
+	ppp_user_data_t *user_data = NULL;
+
+    user_data = (ppp_user_data_t *)luaL_testudata(L, 1, "ppp.trans");
+    if (user_data) {
+    }
+
     return 0;
 }
 
@@ -13,14 +39,18 @@ static const LUA_REG_TYPE pppos_map[] = {
     { LNILKEY, LNILVAL }
 };
 
-LUALIB_API int luaopen_ppp( lua_State *L ) {
-#if !LUA_USE_ROTABLE
-    luaL_newlib(L, ppp_map);
 
-    return 1;
-#else
-	return 0;
-#endif
+//inst map
+static const LUA_REG_TYPE pppos_trans_map[] = {
+    { LSTRKEY( "__metatable" ),  	LROVAL  ( pppos_trans_map ) },
+	{ LSTRKEY( "__index"     ),   	LROVAL  ( pppos_trans_map ) },
+	{ LSTRKEY( "__gc"        ),   	LFUNCVAL  ( pppos_trans_gc ) },
+    { LNILKEY, LNILVAL}
+};
+
+LUALIB_API int luaopen_ppp( lua_State *L ) {
+    luaL_newmetarotable(L,"ppp.trans", (void *)pppos_trans_map);
+    return 0;
 }
 
 MODULE_REGISTER_MAPPED(PPP, ppp, pppos_map, luaopen_ppp);
