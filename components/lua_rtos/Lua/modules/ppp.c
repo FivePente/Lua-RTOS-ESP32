077a2dd27d63eaee1e46aa8c64a9c8f75888c6e4
wangjhm <wangjhm@163.com>
2017-06-09 17:13:42 +0800

    ppp

--- components/lua_rtos/Lua/modules/ppp.c
+++ components/lua_rtos/Lua/modules/ppp.c
@@ -80,8 +80,8 @@ GSM_Cmd GSM_MGR_InitCmds[] = {
     },
     {
         //AT+CGDCONT=1,"IP","apn"
-        .cmd = "AT+CGDCONT=1,\"IP\",\"playmetric\"\r",
-        .cmdSize = sizeof("AT+CGDCONT=1,\"IP\",\"playmetric\"\r") - 1,
+        .cmd = "AT+CGDCONT=1,\"IP\",\"uninet\"\r",
+        .cmdSize = sizeof("AT+CGDCONT=1,\"IP\",\"uninet\"\r") - 1,
         .cmdResponseOnOk = GSM_OK_Str,
         .timeoutMs = 3000,
     },
