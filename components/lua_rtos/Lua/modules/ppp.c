a05d233de917ad2591d88d8316cb60fbaab42956
wangjhm <wangjhm@163.com>
2017-06-14 10:15:33 +0800

    ppp

--- components/lua_rtos/Lua/modules/ppp.c
+++ components/lua_rtos/Lua/modules/ppp.c
@@ -1,6 +1,3 @@
-/*
- * pppos
- */
 #include "luartos.h"
 
 #if CONFIG_LUA_RTOS_LUA_USE_PPP
@@ -36,282 +33,375 @@
 #include "lwip/dns.h"
 #include "lwip/pppapi.h"
 
-const char *PPP_User = "";
-const char *PPP_Pass = "";
-const char *PPP_ApnATReq = "AT+CGDCONT=1,\"IP\",\"CMNET\"";
+static uint8_t conn_ok = 0;
 
+/* The examples use simple GSM configuration that you can set via
+   'make menuconfig'.
+ */
 #define BUF_SIZE (1024)
-
-/* UART */
-static int uart_num = 2;
-
-/* The PPP control block */
-static ppp_pcb *ppp;
-
-/* The PPP IP interface */
-struct netif ppp_netif;
-
-static TaskHandle_t xHandle = NULL;
-
-static const char *TAG = "example";
-
-typedef struct {
-    char *cmd;
-    uint16_t cmdSize;
-    char *cmdResponseOnOk;
-    uint32_t timeoutMs;
-} GSM_Cmd;
-
+// *** If not using hw flow control, set it to 38400
+#define UART_BDRATE 115200
 #define GSM_OK_Str "OK"
 
-GSM_Cmd GSM_MGR_InitCmds[] = {
-    {
-        .cmd = "AT\r",
-        .cmdSize = sizeof("AT\r") - 1,
-        .cmdResponseOnOk = GSM_OK_Str,
-        .timeoutMs = 3000,
-    },
-    {
-        .cmd = "ATE0\r",
-        .cmdSize = sizeof("ATE0\r") - 1,
-        .cmdResponseOnOk = GSM_OK_Str,
-        .timeoutMs = 3000,
-    },
-    /*
-    {
-        .cmd = "AT+CGATT=1\r",
-        .cmdSize = sizeof("AT+CGATT=1\r") - 1,
-        .cmdResponseOnOk = GSM_OK_Str,
-        .timeoutMs = 3000,
-    },*/
-    {
-        .cmd = "AT+CPIN?\r",
-        .cmdSize = sizeof("AT+CPIN?\r") - 1,
-        .cmdResponseOnOk = GSM_OK_Str,
-        .timeoutMs = 3000,
-    },
-    {
-        //AT+CGDCONT=1,"IP","playmetric"
-        .cmd = "AT+CGDCONT=1,\"IP\",\"playmetric\"\r",
-        .cmdSize = sizeof("AT+CGDCONT=1,\"IP\",\"playmetric\"\r") - 1,
-        .cmdResponseOnOk = GSM_OK_Str,
-        .timeoutMs = 3000,
-    },
-    /*
-    {
-        .cmd = "AT+CGACT=1,1\r",
-        .cmdSize = sizeof("AT+CGACT=1,1\r") - 1,
-        .cmdResponseOnOk = GSM_OK_Str,
-        .timeoutMs = 3000,
-    },*/
-    {
-        .cmd = "ATDT*99***1#\r",
-        .cmdSize = sizeof("ATDT*99***1#\r") - 1,
-        .cmdResponseOnOk = "CONNECT",
-        .timeoutMs = 30000,
-    }
+
+const char *PPP_User = "";
+const char *PPP_Pass = "";
+
+// UART
+#define UART_GPIO_TX 17
+#define UART_GPIO_RX 16
+
+static int uart_num = 2;
+
+// The PPP control block
+ppp_pcb *ppp;
+
+// The PPP IP interface
+struct netif ppp_netif;
+
+static const char *TAG = "[PPPOS CLIENT]";
+static const char *TIME_TAG = "[SNTP]";
+
+typedef struct
+{
+	char *cmd;
+	uint16_t cmdSize;
+	char *cmdResponseOnOk;
+	uint32_t timeoutMs;
+	uint32_t delayMs;
+}GSM_Cmd;
+
+//--------------------------
+GSM_Cmd GSM_MGR_InitCmds[] =
+{
+		{
+				.cmd = "AT\r\n",
+				.cmdSize = sizeof("AT\r\n")-1,
+				.cmdResponseOnOk = GSM_OK_Str,
+				.timeoutMs = 3000,
+		},
+		{
+				.cmd = "ATZ\r\n",
+				.cmdSize = sizeof("ATZ\r\n")-1,
+				.cmdResponseOnOk = GSM_OK_Str,
+				.timeoutMs = 3000,
+		},
+		{
+				.cmd = "AT+CFUN=4\r\n",
+				.cmdSize = sizeof("ATCFUN=4\r\n")-1,
+				.cmdResponseOnOk = GSM_OK_Str,
+				.timeoutMs = 3000,
+		},
+		{
+				.cmd = "AT+CFUN=1\r\n",
+				.cmdSize = sizeof("ATCFUN=4,0\r\n")-1,
+				.cmdResponseOnOk = GSM_OK_Str,
+				.timeoutMs = 3000,
+		},
+		{
+				.cmd = "ATE0\r\n",
+				.cmdSize = sizeof("ATE0\r\n")-1,
+				.cmdResponseOnOk = GSM_OK_Str,
+				.timeoutMs = 3000,
+		},
+		{
+				.cmd = "AT+CPIN?\r\n",
+				.cmdSize = sizeof("AT+CPIN?\r\n")-1,
+				.cmdResponseOnOk = "CPIN: READY",
+				.timeoutMs = 3000,
+		},
+		{
+				.cmd = "AT+CREG?\r\n",
+				.cmdSize = sizeof("AT+CREG?\r\n")-1,
+				.cmdResponseOnOk = "CREG: 0,1",
+				.timeoutMs = 3000,
+		},
+		{
+				.cmd = "AT+CGDCONT=1,\"IP\",\"myapn\"\r",
+				.cmdSize = sizeof("AT+CGDCONT=1,\"IP\",\"myapn\"\r")-1,
+				.cmdResponseOnOk = GSM_OK_Str,
+				.timeoutMs = 8000,
+		},
+		/*{
+				.cmd = "ATDT*99***1#\r\n",
+				.cmdSize = sizeof("ATDT*99***1#\r\n")-1,
+				.cmdResponseOnOk = "CONNECT",
+				.timeoutMs = 30000,
+		}*/
+		{
+				.cmd = "AT+CGDATA=\"PPP\",1\r\n",
+				.cmdSize = sizeof("AT+CGDATA=\"PPP\",1\r\n")-1,
+				.cmdResponseOnOk = "CONNECT",
+				.timeoutMs = 30000,
+		}
 };
 
 #define GSM_MGR_InitCmdsSize  (sizeof(GSM_MGR_InitCmds)/sizeof(GSM_Cmd))
 
-/* PPP status callback example */
-static void ppp_status_cb(ppp_pcb *pcb, int err_code, void *ctx)
-{
-    struct netif *pppif = ppp_netif(pcb);
-    LWIP_UNUSED_ARG(ctx);
 
-    switch (err_code) {
-    case PPPERR_NONE: {
-        ESP_LOGI(TAG, "status_cb: Connected\n");
+// PPP status callback
+//----------------------------------------------------------------
+static void ppp_status_cb(ppp_pcb *pcb, int err_code, void *ctx) {
+	struct netif *pppif = ppp_netif(pcb);
+	LWIP_UNUSED_ARG(ctx);
+
+	switch(err_code) {
+	case PPPERR_NONE: {
+		ESP_LOGI(TAG,"status_cb: Connected\n");
 #if PPP_IPV4_SUPPORT
-        ESP_LOGI(TAG, "   our_ipaddr  = %s\n", ipaddr_ntoa(&pppif->ip_addr));
-        ESP_LOGI(TAG, "   his_ipaddr  = %s\n", ipaddr_ntoa(&pppif->gw));
-        ESP_LOGI(TAG, "   netmask     = %s\n", ipaddr_ntoa(&pppif->netmask));
-#endif /* PPP_IPV4_SUPPORT */
+		ESP_LOGI(TAG,"   ipaddr    = %s\n", ipaddr_ntoa(&pppif->ip_addr));
+		ESP_LOGI(TAG,"   gateway   = %s\n", ipaddr_ntoa(&pppif->gw));
+		ESP_LOGI(TAG,"   netmask   = %s\n", ipaddr_ntoa(&pppif->netmask));
+#endif
+
 #if PPP_IPV6_SUPPORT
-        ESP_LOGI(TAG, "   our6_ipaddr = %s\n", ip6addr_ntoa(netif_ip6_addr(pppif, 0)));
-#endif /* PPP_IPV6_SUPPORT */
-        break;
-    }
-    case PPPERR_PARAM: {
-        ESP_LOGE(TAG, "status_cb: Invalid parameter\n");
-        break;
-    }
-    case PPPERR_OPEN: {
-        ESP_LOGE(TAG, "status_cb: Unable to open PPP session\n");
-        break;
-    }
-    case PPPERR_DEVICE: {
-        ESP_LOGE(TAG, "status_cb: Invalid I/O device for PPP\n");
-        break;
-    }
-    case PPPERR_ALLOC: {
-        ESP_LOGE(TAG, "status_cb: Unable to allocate resources\n");
-        break;
-    }
-    case PPPERR_USER: {
-        ESP_LOGE(TAG, "status_cb: User interrupt\n");
-        break;
-    }
-    case PPPERR_CONNECT: {
-        ESP_LOGE(TAG, "status_cb: Connection lost\n");
-        break;
-    }
-    case PPPERR_AUTHFAIL: {
-        ESP_LOGE(TAG, "status_cb: Failed authentication challenge\n");
-        break;
-    }
-    case PPPERR_PROTOCOL: {
-        ESP_LOGE(TAG, "status_cb: Failed to meet protocol\n");
-        break;
-    }
-    case PPPERR_PEERDEAD: {
-        ESP_LOGE(TAG, "status_cb: Connection timeout\n");
-        break;
-    }
-    case PPPERR_IDLETIMEOUT: {
-        ESP_LOGE(TAG, "status_cb: Idle Timeout\n");
-        break;
-    }
-    case PPPERR_CONNECTTIME: {
-        ESP_LOGE(TAG, "status_cb: Max connect time reached\n");
-        break;
-    }
-    case PPPERR_LOOPBACK: {
-        ESP_LOGE(TAG, "status_cb: Loopback detected\n");
-        break;
-    }
-    default: {
-        ESP_LOGE(TAG, "status_cb: Unknown error code %d\n", err_code);
-        break;
-    }
-    }
+		ESP_LOGI(TAG,"   ip6addr   = %s\n", ip6addr_ntoa(netif_ip6_addr(pppif, 0)));
+#endif
+		conn_ok = 1;
+		break;
+	}
+	case PPPERR_PARAM: {
+		ESP_LOGE(TAG,"status_cb: Invalid parameter\n");
+		break;
+	}
+	case PPPERR_OPEN: {
+		ESP_LOGE(TAG,"status_cb: Unable to open PPP session\n");
+		break;
+	}
+	case PPPERR_DEVICE: {
+		ESP_LOGE(TAG,"status_cb: Invalid I/O device for PPP\n");
+		break;
+	}
+	case PPPERR_ALLOC: {
+		ESP_LOGE(TAG,"status_cb: Unable to allocate resources\n");
+		break;
+	}
+	case PPPERR_USER: {
+		ESP_LOGE(TAG,"status_cb: User interrupt\n");
+		break;
+	}
+	case PPPERR_CONNECT: {
+		ESP_LOGE(TAG,"status_cb: Connection lost\n");
+		conn_ok = 0;
+		break;
+	}
+	case PPPERR_AUTHFAIL: {
+		ESP_LOGE(TAG,"status_cb: Failed authentication challenge\n");
+		break;
+	}
+	case PPPERR_PROTOCOL: {
+		ESP_LOGE(TAG,"status_cb: Failed to meet protocol\n");
+		break;
+	}
+	case PPPERR_PEERDEAD: {
+		ESP_LOGE(TAG,"status_cb: Connection timeout\n");
+		break;
+	}
+	case PPPERR_IDLETIMEOUT: {
+		ESP_LOGE(TAG,"status_cb: Idle Timeout\n");
+		break;
+	}
+	case PPPERR_CONNECTTIME: {
+		ESP_LOGE(TAG,"status_cb: Max connect time reached\n");
+		break;
+	}
+	case PPPERR_LOOPBACK: {
+		ESP_LOGE(TAG,"status_cb: Loopback detected\n");
+		break;
+	}
+	default: {
+		ESP_LOGE(TAG,"status_cb: Unknown error code %d\n", err_code);
+		break;
+	}
+	}
 
-    /*
-     * This should be in the switch case, this is put outside of the switch
-     * case for example readability.
-     */
+	/*
+	 * This should be in the switch case, this is put outside of the switch
+	 * case for example readability.
+	 */
 
-    if (err_code == PPPERR_NONE) {
-        return;
-    }
+	if (err_code == PPPERR_NONE) {
+		return;
+	}
 
-    /* ppp_close() was previously called, don't reconnect */
-    if (err_code == PPPERR_USER) {
-        /* ppp_free(); -- can be called here */
-        return;
-    }
-
-    /*
-     * Try to reconnect in 30 seconds, if you need a modem chatscript you have
-     * to do a much better signaling here ;-)
-     */
-    /*ppp_connect(pcb, 30);
-     OR ppp_listen(pcb); */
+	/* ppp_close() was previously called, don't reconnect */
+	if (err_code == PPPERR_USER) {
+		/* ppp_free(); -- can be called here */
+		return;
+	}
 }
 
-
-static u32_t ppp_output_callback(ppp_pcb *pcb, u8_t *data, u32_t len, void *ctx)
-{
-    ESP_LOGI(TAG, "PPP tx len %d", len);
-    return uart_write_bytes(uart_num, (const char *)data, len);
+//--------------------------------------------------------------------------------
+static u32_t ppp_output_callback(ppp_pcb *pcb, u8_t *data, u32_t len, void *ctx) {
+	// *** Handle sending to GSM modem ***
+	uint32_t ret = uart_write_bytes(uart_num, (const char*)data, len);
+    uart_wait_tx_done(uart_num, 10 / portTICK_RATE_MS);
+    return ret;
 }
 
-#define UART1_TX_PIN 17
-#define UART1_RX_PIN 16
-
+//-----------------------------
 static void pppos_client_task()
 {
-    char *data = (char *) malloc(BUF_SIZE);
-    uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-    };
+	uint8_t init_ok = 1;
+	int pass = 0;
+	char sresp[256] = {'\0'};
 
-    uart_driver_delete(uart_num);
+    char* data = (char*) malloc(BUF_SIZE);
+	char PPP_ApnATReq[sizeof(CONFIG_GSM_APN)+8];
+	
+	uart_config_t uart_config = {
+			.baud_rate = UART_BDRATE,
+			.data_bits = UART_DATA_8_BITS,
+			.parity = UART_PARITY_DISABLE,
+			.stop_bits = UART_STOP_BITS_1,
+			.flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+	};
+	//Configure UART1 parameters
+	uart_param_config(uart_num, &uart_config);
+	//Set UART1 pins(TX, RX, RTS, CTS)
+	uart_set_pin(uart_num, UART_GPIO_TX, UART_GPIO_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+	uart_driver_install(uart_num, BUF_SIZE * 2, BUF_SIZE * 2, 0, NULL, 0);
 
-    //Configure UART1 parameters
-    uart_param_config(uart_num, &uart_config);
+	// Set APN from config
+	sprintf(PPP_ApnATReq, "AT+CGDCONT=1,\"IP\",\"%s\"\r\n", "playmetric");
+	GSM_MGR_InitCmds[7].cmd = PPP_ApnATReq;
+	GSM_MGR_InitCmds[7].cmdSize = strlen(PPP_ApnATReq);
 
-    // Configure UART1 pins (as set in example's menuconfig)
-    ESP_LOGI(TAG, "Configuring UART1 GPIOs: TX:%d RX:%d RTS:%d CTS: %d",
-             UART1_TX_PIN, UART1_RX_PIN, 0, 0);
-    uart_set_pin(uart_num, UART1_TX_PIN, UART1_RX_PIN, 0, 0);
-    uart_driver_install(uart_num, BUF_SIZE * 2, BUF_SIZE * 2, 0, NULL, 0);
+	ESP_LOGI(TAG,"Gsm init start");
+	// *** Disconnect if connected ***
+	vTaskDelay(1000 / portTICK_PERIOD_MS);
+	while (uart_read_bytes(uart_num, (uint8_t*)data, BUF_SIZE, 100 / portTICK_RATE_MS)) {
+		vTaskDelay(100 / portTICK_PERIOD_MS);
+	}
+	uart_write_bytes(uart_num, "+++", 3);
+    uart_wait_tx_done(uart_num, 10 / portTICK_RATE_MS);
+	vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-    while (1) {
-        //init gsm
-        int gsmCmdIter = 0;
-        while (1) {
-            ESP_LOGI(TAG, "%s", GSM_MGR_InitCmds[gsmCmdIter].cmd);
-            uart_write_bytes(uart_num, (const char *)GSM_MGR_InitCmds[gsmCmdIter].cmd,
-                             GSM_MGR_InitCmds[gsmCmdIter].cmdSize);
+	conn_ok = 98;
+	while(1)
+	{
+		init_ok = 1;
+		// Init gsm
+		int gsmCmdIter = 0;
+		while(1)
+		{
+			// ** Send command to GSM
+			memset(sresp, 0, 256);
+			for (int i=0; i<255;i++) {
+				if ((GSM_MGR_InitCmds[gsmCmdIter].cmd[i] >= 0x20) && (GSM_MGR_InitCmds[gsmCmdIter].cmd[i] < 0x80)) {
+					sresp[i] = GSM_MGR_InitCmds[gsmCmdIter].cmd[i];
+					sresp[i+1] = 0;
+				}
+				if (GSM_MGR_InitCmds[gsmCmdIter].cmd[i] == 0) break;
+			}
+			printf("[GSM INIT] >Cmd: [%s]\r\n", sresp);
+			vTaskDelay(100 / portTICK_PERIOD_MS);
+			while (uart_read_bytes(uart_num, (uint8_t*)data, BUF_SIZE, 100 / portTICK_RATE_MS)) {
+				vTaskDelay(100 / portTICK_PERIOD_MS);
+			}
+			uart_write_bytes(uart_num, (const char*)GSM_MGR_InitCmds[gsmCmdIter].cmd,
+					GSM_MGR_InitCmds[gsmCmdIter].cmdSize);
+            uart_wait_tx_done(uart_num, 10 / portTICK_RATE_MS);
 
+            // ** Wait for and check the response
             int timeoutCnt = 0;
-            while (1) {
-                memset(data, 0, BUF_SIZE);
-                int len = uart_read_bytes(uart_num, (uint8_t *)data, BUF_SIZE, 500 / portTICK_RATE_MS);
-                if (len > 0) {
-                    ESP_LOGI(TAG, "%s", data);
-                }
+			memset(sresp, 0, 256);
+			int idx = 0;
+			int tot = 0;
+			while(1)
+			{
+				memset(data, 0, BUF_SIZE);
+				int len = 0;
+				len = uart_read_bytes(uart_num, (uint8_t*)data, BUF_SIZE, 10 / portTICK_RATE_MS);
+				if (len > 0) {
+					for (int i=0; i<len;i++) {
+						if (idx < 255) {
+							if ((data[i] >= 0x20) && (data[i] < 0x80)) {
+								sresp[idx++] = data[i];
+							}
+							else sresp[idx++] = 0x2e;
+							sresp[idx] = 0;
+						}
+					}
+					tot += len;
+				}
+				else {
+					if (tot > 0) {
+						printf("[GSM INIT] <Resp: [%s], %d\r\n", sresp, tot);
+						if (strstr(sresp, GSM_MGR_InitCmds[gsmCmdIter].cmdResponseOnOk) != NULL) {
+							break;
+						}
+						else {
+							printf("           Wrong response, expected [%s]\r\n", GSM_MGR_InitCmds[gsmCmdIter].cmdResponseOnOk);
+							init_ok = 0;
+							break;
+						}
+					}
+				}
+				timeoutCnt += 10;
 
-                timeoutCnt += 500;
-                if (strstr(data, GSM_MGR_InitCmds[gsmCmdIter].cmdResponseOnOk) != NULL) {
-                    break;
-                }
+				if (timeoutCnt > GSM_MGR_InitCmds[gsmCmdIter].timeoutMs)
+				{
+					printf("[GSM INIT] No response, Gsm Init Error\r\n");
+					init_ok = 0;
+					break;
+				}
+			}
+			if (init_ok == 0) {
+				// No response or not as expected
+				vTaskDelay(5000 / portTICK_PERIOD_MS);
+				init_ok = 1;
+				gsmCmdIter = 0;
+				continue;
+			}
 
-                if (timeoutCnt > GSM_MGR_InitCmds[gsmCmdIter].timeoutMs) {
-                    ESP_LOGE(TAG, "Gsm Init Error");
-                    vTaskDelete(NULL);
-                    return;
-                }
-            }
-            gsmCmdIter++;
+			if (gsmCmdIter == 2) vTaskDelay(500 / portTICK_PERIOD_MS);
+			if (gsmCmdIter == 3) vTaskDelay(1500 / portTICK_PERIOD_MS);
+			gsmCmdIter++;
+			if (gsmCmdIter >= GSM_MGR_InitCmdsSize) break; // All init commands sent
+			if ((pass) && (gsmCmdIter == 2)) gsmCmdIter = 4;
+			if (gsmCmdIter == 6) pass++;
+		}
 
-            if (gsmCmdIter >= GSM_MGR_InitCmdsSize) {
-                break;
-            }
-        }
+		ESP_LOGI(TAG,"Gsm init end");
 
-        ESP_LOGI(TAG, "Gsm init end");
+		if (conn_ok == 98) {
+			// After first successful initialization
+			ppp = pppapi_pppos_create(&ppp_netif,
+					ppp_output_callback, ppp_status_cb, NULL);
 
-        ppp = pppapi_pppos_create(&ppp_netif,
-                                  ppp_output_callback, ppp_status_cb, NULL);
+			ESP_LOGI(TAG,"After pppapi_pppos_create");
 
-        ESP_LOGI(TAG, "After pppapi_pppos_create");
+			if(ppp == NULL)	{
+				ESP_LOGE(TAG, "Error init pppos");
+				return;
+			}
+		}
+		conn_ok = 99;
+		pppapi_set_default(ppp);
+		pppapi_set_auth(ppp, PPPAUTHTYPE_PAP, PPP_User, PPP_Pass);
+		//pppapi_set_auth(ppp, PPPAUTHTYPE_NONE, PPP_User, PPP_Pass);
+		pppapi_connect(ppp, 0);
 
-        if (ppp == NULL) {
-            ESP_LOGE(TAG, "Error init pppos");
-            vTaskDelete(NULL);
-            return;
-        }
-
-        pppapi_set_default(ppp);
-
-        ESP_LOGI(TAG, "After pppapi_set_default");
-
-        pppapi_set_auth(ppp, PPPAUTHTYPE_PAP, PPP_User, PPP_Pass);
-
-        ESP_LOGI(TAG, "After pppapi_set_auth");
-
-        pppapi_connect(ppp, 0);
-
-        ESP_LOGI(TAG, "After pppapi_connect");
-
-        while (1) {
-            memset(data, 0, BUF_SIZE);
-            int len = uart_read_bytes(uart_num, (uint8_t *)data, BUF_SIZE, 10 / portTICK_RATE_MS);
-            if (len > 0) {
-                ESP_LOGI(TAG, "PPP rx len %d", len);
-                pppos_input_tcpip(ppp, (u8_t *)data, len);
-            }
-        }
-    }
+		// *** Handle GSM modem responses ***
+		while(1) {
+			memset(data, 0, BUF_SIZE);
+			int len = uart_read_bytes(uart_num, (uint8_t*)data, BUF_SIZE, 30 / portTICK_RATE_MS);
+			if(len > 0)	{
+				pppos_input_tcpip(ppp, (u8_t*)data, len);
+			}
+			// Check if disconnected
+			if (conn_ok == 0) {
+				ESP_LOGE(TAG, "Disconnected, trying again...");
+				pppapi_close(ppp, 0);
+				gsmCmdIter = 0;
+				conn_ok = 89;
+				vTaskDelay(1000 / portTICK_PERIOD_MS);
+				break;
+			}
+		}
+	}
 }
-
 
 static int ppp_task_setup(lua_State* L){
     tcpip_adapter_init();
