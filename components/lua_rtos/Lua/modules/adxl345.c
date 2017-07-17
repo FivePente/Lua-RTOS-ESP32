617b5c0dcc52afa81a83dd707f72790a7a25b998
wangjhm <wangjhm@163.com>
2017-07-17 10:56:20 +0800

    ...

--- components/lua_rtos/Lua/modules/adxl345.c
+++ components/lua_rtos/Lua/modules/adxl345.c
@@ -100,32 +100,32 @@ static int adxl345_read(lua_State* L) {
     char start_addr = 0x32;
 
     if ((error = i2c_start(user_data->unit, &user_data->transaction))) {
-        printf("adxl345 read error 1\n")
+        printf("adxl345 read error 1\n");
     	return luaL_driver_error(L, error);
     }
 
 	if ((error = i2c_write_address(user_data->unit, &user_data->transaction, adxl345_i2c_addr, false))) {
-        printf("adxl345 read error 2\n")
+        printf("adxl345 read error 2\n");
     	return luaL_driver_error(L, error);
     }
 
     if ((error = i2c_write(user_data->unit, &user_data->transaction, &start_addr , sizeof(uint8_t)))) {
-    	printf("adxl345 read error 3\n")
+    	printf("adxl345 read error 3\n");
         return luaL_driver_error(L, error);
     }
 
     if ((error = i2c_start(user_data->unit, &user_data->transaction))) {
-    	printf("adxl345 read error 4\n")
+    	printf("adxl345 read error 4\n");
         return luaL_driver_error(L, error);
     }
 
 	if ((error = i2c_write_address(user_data->unit, &user_data->transaction, adxl345_i2c_addr, true))) {
-    	printf("adxl345 read error 5\n")
+    	printf("adxl345 read error 5\n");
         return luaL_driver_error(L, error);
     }
 
     if ((error = i2c_read(user_data->unit, &user_data->transaction, data, 6))) {
-        printf("adxl345 read error6\n")
+        printf("adxl345 read error6\n");
     	return luaL_driver_error(L, error);
     }
 
@@ -135,7 +135,7 @@ static int adxl345_read(lua_State* L) {
     //}
 
     if ((error = i2c_stop(user_data->unit, &user_data->transaction))) {
-    	printf("adxl345 read error 7\n")
+    	printf("adxl345 read error 7\n");
         return luaL_driver_error(L, error);
     }
 
