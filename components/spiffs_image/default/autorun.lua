1969d6dbca70ee88c77fa86d8f8c4a08c8cb134a
wangjhm <wangjhm@163.com>
2017-07-21 23:32:21 +0800

    ....

--- components/spiffs_image/default/autorun.lua
+++ components/spiffs_image/default/autorun.lua
@@ -47,10 +47,10 @@ local ver = 1.0
 
 function initI2C() 
     cd = adxl345.init(i2c.I2C0 , i2c.MASTER , 400 , pio.GPIO18 , pio.GPIO19)
-    cd:write(0x2D , 0x08)
-    cd:write(0x31 , 0x28)
-    cd:write(0x2C , 0x0C)
-    cd:write(0x38 , 0xA0)
+    cd:write(0x2D , 0x08) 
+    cd:write(0x31 , 0x28) --0xA8
+    cd:write(0x2C , 0x0C) --0x0C
+    --cd:write(0x38 , 0xA0)
 
     ad = vl53l0x.init(i2c.I2C0 , i2c.MASTER , 400 , 0x29 , pio.GPIO18 , pio.GPIO19)
     ad:startRanging(2)
