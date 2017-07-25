4fbb36de61fb08ea4e2e3d0abdbfde61174489c0
wangjhm <wangjhm@163.com>
2017-07-25 20:51:05 +0800

    ....

--- components/lua_rtos/Lua/modules/adxl345.c
+++ components/lua_rtos/Lua/modules/adxl345.c
@@ -99,10 +99,10 @@ static int queue_receive(lua_State* L){
 	user_data = (x_queue_t *)luaL_checkudata(L, 1, "adxl345.queue");
     luaL_argcheck(L, user_data, 1, "adxl345 transaction expected");
 
-    char *luaMsg;
+    char luaMsg;
 
-    if (xQueueReceive( user_data->msgQueue, luaMsg , 100/portTICK_RATE_MS ) == pdPASS){
-        lua_pushstring(L, luaMsg);
+    if (xQueueReceive( user_data->msgQueue, &luaMsg , 100/portTICK_RATE_MS ) == pdPASS){
+        lua_pushstring(L, &luaMsg);
     }else{
         lua_pushnil(L);
     }
