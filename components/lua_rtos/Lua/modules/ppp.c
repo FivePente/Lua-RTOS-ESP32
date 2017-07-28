760e246551a0b6d67545833163421711db0b49ce
wangjhm <wangjhm@163.com>
2017-07-28 09:24:32 +0800

    。。。

--- components/lua_rtos/Lua/modules/ppp.c
+++ components/lua_rtos/Lua/modules/ppp.c
@@ -96,14 +96,14 @@ GSM_Cmd GSM_MGR_InitCmds[] =
 				.cmdSize = sizeof("ATZ\r\n")-1,
 				.cmdResponseOnOk = GSM_OK_Str,
 				.timeoutMs = 3000,
-		},
+		},/*
 		{
 				.cmd = "AT+CCID\r\n",
 				.cmdSize = sizeof("AT+CCID\r\n")-1,
 				.cmdResponseOnOk = GSM_OK_Str,
 				.timeoutMs = 3000,
 		},
-		/*{
+		{
 				.cmd = "AT+CFUN=4\r\n",
 				.cmdSize = sizeof("ATCFUN=4\r\n")-1,
 				.cmdResponseOnOk = GSM_OK_Str,
